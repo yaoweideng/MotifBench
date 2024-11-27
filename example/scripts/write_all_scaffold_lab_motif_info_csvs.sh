be8ab2f2d204e5ae53b30b45bcf8ee9329d96af0
blt2114 <me@briantrippe.com>
2024-11-27 11:37:36 -0800

    updated RFdiffusion scripts and metadata

--- example/scripts/write_all_scaffold_lab_motif_info_csvs.sh
+++
@@ -1,4 +0,0 @@
-rfdiffusion_outputs_base=/projects/m000018/projects/RFDiffusion_benchmarking/test4/
-ls $rfdiffusion_outputs_base | while read l; do
-	python write_scaffold_lab_motif_info_csv.py $rfdiffusion_outputs_base/$l  $rfdiffusion_outputs_base/$l/motif_info.csv  ../motif_specs_with_contigs_for_motif_files.csv
-done
