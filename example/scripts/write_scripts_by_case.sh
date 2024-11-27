c6c0d64da2bf8b7ca12bc59df71dd119ca30d768
blt2114 <me@briantrippe.com>
2024-11-26 17:43:05 -0800

    added for making rfdiffusion scripts

--- example/scripts/write_scripts_by_case.sh
+++ example/scripts/write_scripts_by_case.sh
@@ -1,29 +1,52 @@
 #!/bin/bash
-path=/users/btrippe/projects/motif_scaffolding_benchmark/RFDiffusion_baseline/
-scripts_dir=$path/scripts/run_scripts/
-motif_dir=/users/btrippe/projects/motif_scaffolding_benchmark/motif_pdbs/
-csv_path=$path/motif_specs_with_contigs_for_motif_files.csv
-model_dir=/users/btrippe/projects/motif_scaffolding_benchmark/RFDiffusion_baseline/RFdiffusion/models/
-apptainer_dir=/projects/m000018/docker/
+benchmark_dir=/home/users/btrippe/projects/motif_scaffolding_benchmark/
+example_dir=$benchmark_dir/example/
+output_base=/home/groups/btrippe/projects/motif_scaffolding/2024_11_26/rfdiffusion/
+motif_dir=$benchmark_dir/motif_pdbs/
+rfdiffusion_source_dir=/home/users/btrippe/projects/motif_scaffolding_benchmark_original/RFDiffusion_baseline/RFdiffusion/
 
+csv_path=$example_dir/contig_specifications.csv
+scripts_dir=$example_dir/scripts/run_scripts/
+model_dir=$rfdiffusion_source_dir/models/
+log_dir=$output_base/logs/
+mkdir -p $log_dir
 
 cat $csv_path | while read l; do
     IFS=',' read -r motif_name length contig <<< "$l"
     contig=`echo $contig | tr ";" "/"`
     script_fn=$scripts_dir/run_$motif_name.sh
-    echo "#!/bin/bash" > $script_fn
-    echo "cd $apptainer_dir" >> $script_fn
-    output_dir=/projects/m000018/projects/RFDiffusion_benchmarking/test4/$motif_name
+    output_dir=$output_base/$motif_name/
     mkdir -p $output_dir
-    output_prefix=$output_dir/${motif_name:3:7}
+    output_prefix=$output_dir/$motif_name
     
     # Command including apptainer call
     #cmd="echo python3 $path/run_inference.py inference.output_prefix=$output_prefix inference.input_pdb=$motif_dir/$motif_name.pdb contigmap.contigs=[$contig] contigmap.length=$length-$length inference.num_designs=100 inference.model_directory_path=$model_dir inference.write_trajectory=False | bash $path/apptainer_shell.sh" 
-    log_dir=/projects/m000018/projects/RFDiffusion_benchmarking/logs/test4/
+    slurm_header="#!/usr/bin/bash
+#SBATCH --time=12:00:00
+#SBATCH -p gpu
+#SBATCH --gpus=1
+#SBATCH -c 4"
+    printf "%s\n" "$slurm_header" > $script_fn
+    echo "export NGC_API_KEY=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" >> $script_fn
+    echo "docker_sifs_path=$GROUP_HOME/docker/" >> $script_fn
+    echo "base_dir=/home/users/btrippe/projects/motif_scaffolding_benchmark_original/RFDiffusion_baseline/RFdiffusion/" >> $script_fn
     std_out=$log_dir/$motif_name.out
     std_err=$log_dir/$motif_name.err
-    
-    cmd="python3 $path/run_inference.py inference.output_prefix=$output_prefix inference.input_pdb=$motif_dir/$motif_name.pdb contigmap.contigs=[$contig] contigmap.length=$length-$length inference.num_designs=100 inference.model_directory_path=$model_dir inference.write_trajectory=False >$std_out 2>$std_err &" 
-    echo $cmd >> $script_fn
+    cmd="apptainer exec --nv \\
+      --bind \$base_dir/models:\$base_dir/models \\
+      --bind \$base_dir/inputs:\$base_dir/inputs \\
+      --bind \$base_dir/outputs:\$base_dir/outputs \\
+      --env NGC_API_KEY=\$NGC_API_KEY \\
+      \$docker_sifs_path/rfdiffusion.sif \\
+      python3 $example_dir/run_inference.py \\
+            inference.output_prefix=$output_prefix \\
+            inference.input_pdb=$motif_dir/$motif_name.pdb \\
+            contigmap.contigs=[$contig] \\
+            contigmap.length=$length-$length \\
+            inference.num_designs=100 \\
+            inference.model_directory_path=$model_dir \\
+            inference.write_trajectory=False >$std_out 2>$std_err"
+    printf "%s\n" "$cmd" >> $script_fn
+
     chmod a+x $script_fn
 done
