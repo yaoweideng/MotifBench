b0c6f5fe078ff456c93e98cdaa9b9047e8354d18
blt2114 <me@briantrippe.com>
2024-11-30 08:21:21 -0800

    updated scripts and submodule

--- scripts/collect_summaries.sh
+++ scripts/collect_summaries.sh
@@ -9,26 +9,25 @@ fi
 # Input file containing the list of summary file paths
 eval_dir=$1
 all_summaries_fn=$eval_dir/all_summary_paths.txt
-ls $eval_dir/*/summary.txt > $all_summaries_fn
+ls $eval_dir/*/esm_summary.txt > $all_summaries_fn
 
 # Output CSV file
 output_file=$2
 
 # Initialize the output CSV file with headers
-echo "Path,Protein,Num_Solutions,Novelty,Success_rate" > "$output_file"
+echo "Protein,Num_Solutions,Novelty,Success_rate" > "$output_file"
 
 # Loop through each summary file path in the input file
 while IFS= read -r summary_file; do
   if [ -f "$summary_file" ]; then
     # Extract relevant fields from the summary file
-    path=$(awk -F': ' '/evaluation results for/ {print $2}' "$summary_file")
     protein=$(awk -F': ' '/Evaluated Protein/ {print $2}' "$summary_file")
-    num_solutions=$(awk -F': ' '/Number of distict solutions/ {print $2}' "$summary_file")
+    num_solutions=$(awk -F': ' '/Number of distinct solutions/ {print $2}' "$summary_file")
     novelty=$(awk -F': ' '/Novelty/ {printf "%.3f", $2}' "$summary_file")
     success_rate=$(awk -F': ' '/Success rate/ {gsub("%", "", $2); printf "%.2f", $2}' "$summary_file")
 
     # Append the extracted information to the output CSV
-    echo "$path,$protein,$num_solutions,$novelty,$success_rate" >> "$output_file"
+    echo "$protein,$num_solutions,$novelty,$success_rate" >> "$output_file"
   else
     echo "Warning: File $summary_file not found. Skipping..."
   fi
