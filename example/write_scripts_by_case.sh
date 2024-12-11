0a3266837f9d2f8b3e560361aef053d9f0846b01
blt2114 <me@briantrippe.com>
2024-12-11 14:48:05 -0800

    cleaned up information on RFdiffusion baseline

--- example/write_scripts_by_case.sh
+++ example/write_scripts_by_case.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
-benchmark_dir=/home/users/btrippe/projects/motif_scaffolding_benchmark/
-output_base=/home/groups/btrippe/projects/motif_scaffolding/2024_12_10/rfdiffusion/
-model_dir=/home/users/btrippe/projects/motif_scaffolding_benchmark_original/RFDiffusion_baseline/RFdiffusion/models/
+
+# Check if a file path is provided as an argument
+if [ "$#" -ne 3 ]; then
+  echo "Usage: $0 config.txt RFdiffusion_dir/ Docker_dir/"
+  exit 1
+fi
+
+# Source config
+config_fn=$1
+source $config_fn
+if [ -z "$scaffold_base_dir" ] || [ -z "$benchmark_dir" ] ; then
+    echo "Error: Configuration file is missing necessary settings."
+    echo "Ensure it includes 'scaffold_base_dir' and 'benchmark_dir'"
+    exit 1
+fi
+
+RFdiffusion_dir=$2
+Docker_dir=$2
+
+model_dir=$Docker_dir/models/
 
 example_dir=$benchmark_dir/example/
 motif_dir=$benchmark_dir/motif_pdbs/
@@ -9,14 +26,14 @@ motif_dir=$benchmark_dir/motif_pdbs/
 csv_path=$example_dir/contig_specifications.csv
 scripts_dir=$example_dir/run_scripts/
 mkdir -p $scripts_dir
-log_dir=$output_base/logs/
+log_dir=$scaffold_base_dir/logs/
 mkdir -p $log_dir
 
 cat $csv_path | tail -n +2 | while read l; do
     IFS=',' read -r motif_name length contig <<< "$l"
     contig=`echo $contig | tr ";" "/"`
     script_fn=$scripts_dir/run_$motif_name.sh
-    output_dir=$output_base/$motif_name/
+    output_dir=$scaffold_base_dir/$motif_name/
     mkdir -p $output_dir
     output_prefix=$output_dir/$motif_name
     
@@ -27,8 +44,8 @@ cat $csv_path | tail -n +2 | while read l; do
 #SBATCH --gpus=1
 #SBATCH -c 4"
     printf "%s\n" "$slurm_header" > $script_fn
-    echo "docker_sifs_path=$GROUP_HOME/docker/" >> $script_fn
-    echo "base_dir=/home/users/btrippe/projects/motif_scaffolding_benchmark_original/RFDiffusion_baseline/RFdiffusion/" >> $script_fn
+    echo "docker_sifs_path=$Docker_dir/" >> $script_fn
+    echo "base_dir=$RFdiffusion_dir" >> $script_fn
     std_out=$log_dir/$motif_name.out
     std_err=$log_dir/$motif_name.err
     cmd="apptainer exec --nv \\
@@ -36,7 +53,7 @@ cat $csv_path | tail -n +2 | while read l; do
       --bind \$base_dir/inputs:\$base_dir/inputs \\
       --bind \$base_dir/outputs:\$base_dir/outputs \\
       \$docker_sifs_path/rfdiffusion.sif \\
-      python3 $example_dir/run_inference.py \\
+      python3 $RFdiffusion_dir/scripts/run_inference.py \\
             inference.output_prefix=$output_prefix \\
             inference.input_pdb=$motif_dir/$motif_name.pdb \\
             contigmap.contigs=[$contig] \\
