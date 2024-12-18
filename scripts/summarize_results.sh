90136248ab402201e15b242755f6ea7a0d995752
Immortals-33 <1332011796@qq.com>
2024-12-19 00:30:21 +0800

    Modify  to meet the new summary output format

--- scripts/summarize_results.sh
+++ scripts/summarize_results.sh
@@ -21,10 +21,10 @@ echo "Problem,Num_Solutions,Novelty,Success_rate" > "$summary_by_case_fn"
 ls $base_output_dir/*/esm_summary.txt | while IFS= read -r summary_file; do
   if [ -f "$summary_file" ]; then
     # Extract relevant fields from the summary file
-    protein=$(awk -F': ' '/Evaluated Protein/ {print $2}' "$summary_file")
-    num_solutions=$(awk -F': ' '/Number of distinct solutions/ {print $2}' "$summary_file")
-    novelty=$(awk -F': ' '/Novelty/ {printf "%.3f", $2}' "$summary_file")
-    success_rate=$(awk -F': ' '/Success rate/ {gsub("%", "", $2); printf "%.2f", $2}' "$summary_file")
+    protein=$(awk '/Evaluated Protein/ {getline; print $2}' "$summary_file")
+    num_solutions=$(awk '/Designability/ {getline; print $2}' "$summary_file")
+    novelty=$(awk '/Diversity/ {getline; print $2}' "$summary_file")
+    success_rate=$(awk '/Success Rate/ {getline; gsub("%", "", $2); print $2}' "$summary_file")
 
     # Append the extracted information to the output CSV
     echo "$protein,$num_solutions,$novelty,$success_rate" >> $summary_by_case_fn
